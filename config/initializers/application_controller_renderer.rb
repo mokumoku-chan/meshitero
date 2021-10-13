49829cd4d64cb755c45e084f90eaf68fab6663b5
mokumoku-chan <maho.kumo1997@gmail.com>
2021-10-13 13:47:12 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd885e861306aa726afa8b22b9a8b8cc5ca5c32faba42f9d2c1e38bc3f8bc58d9dc77be0e53a65180ac4d54e6d46d7a30886bdbd5f8d9f712eabcd9d5b1faba2c'
