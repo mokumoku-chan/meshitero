ad2b2656775e0b4ebbc3ae3de14f5875983424f7
mokumoku-chan <maho.kumo1997@gmail.com>
2021-10-16 06:23:56 +0000

    [Finish] 4-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :post_images, only:[:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+
+  end
+
+  resources :users, only: [:show, :edit, :update]
+
 end
